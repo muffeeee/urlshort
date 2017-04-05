4087161939ff78c0a639b11c722f77228221bbe7
muffe <muffeeee@users.noreply.github.com>
2017-04-05 10:15:33 +0200

    custom char generation
    added a custom character generation so url's will be shorter

--- webSchema.js
+++ webSchema.js
@@ -2,10 +2,21 @@ var mongoose = require("mongoose");
 var shortid = require('shortid');
 var Schema = mongoose.Schema;
 
+var charset = ["A", "a", "B", "b", "C", "c", "D", "d", "E", "e", "F", "f", "G", "g", "H", "h", "I", "i", "J", "j", "K", "k", "L", "l", "M", "m", "N", "n", "O", "o", "P", "p", "Q", "q", "R", "r", "S", "s", "T", "t", "U", "u", "V", "v", "W", "w", "X", "x", "Y", "y", "Z", "z"]
+function gen(len) {
+        var gen = "";
+        for (i=0; i<len; i++) {
+                char = Math.floor(Math.random()*52);
+                gen += charset[char];
+        }
+        return gen;
+}
+
+
 var webSchema = new Schema({
   _id: {
     type: String,
-    'default': shortid.generate
+    'default': gen(4)
   },
   url: String,
 })
